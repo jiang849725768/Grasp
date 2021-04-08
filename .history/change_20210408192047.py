39b152266de328f553424b7867c4402ca73dd7b2
jiang849725768 <jiang849725768@buaa.edu.cn>
2021-04-08 19:21:29 +0800

    change_new

--- .history/change_20210408192047.py
+++ .history/change_20210408192047.py
@@ -3,7 +3,7 @@ import numpy as np
 print('test')
 # use [:, np.newaxis] to transform from row vector to col vector
 position = np.array(
-    [0.03982763511251628, -0.8316034171999891, 0.8334303864998827])[:, np.newaxis]
+    [0.03982763511251628, -0.8316034171999891, 0.8334303864998827])[:, np.newaxis].T
 share_vector = np.array([0, 0, 0, 1], dtype=float)[np.newaxis, :]
 print('position:\n', position)
 r = R.from_quat([0.1225787223613701, -0.7441192315128069, 0.642821221041933, 0.1343201544638865])
